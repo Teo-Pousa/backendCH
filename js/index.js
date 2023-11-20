30732a82d939f677fa4ad7f4042fcd032c690861
teopousa <teopousa@live.com>
2023-11-20 17:59:19 -0300

    TP 1 Backend CH

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
-const teo = facha
+class Products
+{
+    elements=[];
+    static id;
+    constructor()
+    {this.id=0;}
+    addProduct(title,description,price,thumbnail,code,stock)
+    {
+        const encontrado = this.elements.findIndex(e=>e.code===code);
+        if (encontrado ===-1) {
+            const data={title,description,price,thumbnail,code,stock,id:this.id++}
+            this.elements.push(data);
+        }
+        else console.log("El codigo digitado ya existe.")
+    }
+    getProductById(id){
+        const data=find(e=>e.id===id);
+        return data;
+    }
+}
 
-facha
+const datos=new Products();
+datos.addProduct("Poner","Nuevos",100,"Datos","Aca",546);
+console.log(datos);
+
