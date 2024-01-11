a964e01798866ba1d5bf19f3aa3892d2b8fb7d4c
Tu Nombre <teopousa@live.com>
2024-01-10 22:00:34 -0300

    correción de ultimo trabajo

--- router/index.js
+++ router/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const productsController = require('../controllers/productsController');
 const cartsController = require('../controllers/cartsController');
 
+const CartManager = require('./cartManager');
+const CartManager = new CartManager('cart.json');
+
 const router = express.Router();
 
 router.get('/', productsController.getProducts);
@@ -13,5 +16,61 @@ router.delete('/:pid', productsController.deleteProduct);
 router.post('/:cid/product/:pid', cartsController.addProductToCart);
 router.get('/:cid', cartsController.getCart);
 router.post('/', cartsController.addProductToCart);
+
+const expressHandlebars = require('express-handlebars');
+const productsRouter = require('../router/index');
+// Configuración de Handlebars
+app.engine('handlebars', expressHandlebars.engine());
+app.set('view engine', 'handlebars');
+app.set('views', process.cwd() + '/src/views');
+// Rutas principales
+app.use('/api/products', productsRouter);
+
+
+const Products = require('./productManager');
+const productManager = new Products('products.json');
+// Vista Home con todos los productos
+app.get('/', async (req, res) => {
+    const products = await productManager.getProducts();
+    res.render('home', { products });
+});
+// Vista Home
+app.get('/home', async (req, res) => {
+    try {
+        const products = await productManager.getProducts();
+        res.render('home', { products });
+    } catch (error) {
+        console.error('Error al obtener productos:', error);
+        res.status(500).send('Error interno del servidor.');
+    }
+});
+
+app.get('/', (req, res) => {
+    res.render('home', { layout: 'layouts/main' });
+});
+
+let productsData = [];
+try {
+    const rawData = fs.readFileSync('products.json');
+    productsData = JSON.parse(rawData);
+} catch (error) {
+    console.error('Error al leer el archivo products.json:', error);
+}
+
+app.get('/realtimeproducts', (req, res) => {
+    res.render('realTimeProducts', { products: productsData });
+});
+
+
+// Vista RealTimeProducts con WebSockets
+app.get('/realTimeProducts', async (req, res) => {
+    const products = await productManager.getProducts();
+    res.render('realTimeProducts', { products });
+});
+app.get('/websockets', (req, res) => {
+    res.render('websockets', { layout: 'layouts/main' });
+});
+
+
 
 module.exports = router;
--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 
 
 const express = require('express');
-const expressHandlebars = require('express-handlebars');
+
 const http = require('http');
 const socketIo = require('socket.io');
-const Products = require('./productManager');
-const productsRouter = require('../router/index');
+
 
 const app = express();
 const server = http.createServer(app);
@@ -13,17 +12,6 @@ const io = socketIo(server);
 
 const PORT = 8080;
 
-const productManager = new Products('products.json');
-
-const CartManager = require('./cartManager');
-const CartManager = new CartManager('cart.json');
-
-// Configuración de Handlebars
-app.engine('handlebars', expressHandlebars.engine());
-app.set('view engine', 'handlebars');
-app.set('views', process.cwd() + '/src/views');
-// Rutas principales
-app.use('/api/products', productsRouter);
 
 // Socket.io configuration
 io.on('connection', (socket) => {
@@ -34,47 +22,6 @@ io.on('connection', (socket) => {
 });
 // Cuando agregues o elimines un producto
 io.emit('productUpdate', 'Actualización de productos');
-// Vista Home con todos los productos
-app.get('/', async (req, res) => {
-    const products = await productManager.getProducts();
-    res.render('home', { products });
-});
-// Vista Home
-app.get('/home', async (req, res) => {
-    try {
-        const products = await productManager.getProducts();
-        res.render('home', { products });
-    } catch (error) {
-        console.error('Error al obtener productos:', error);
-        res.status(500).send('Error interno del servidor.');
-    }
-});
-
-app.get('/', (req, res) => {
-    res.render('home', { layout: 'layouts/main' });
-});
-
-let productsData = [];
-try {
-    const rawData = fs.readFileSync('products.json');
-    productsData = JSON.parse(rawData);
-} catch (error) {
-    console.error('Error al leer el archivo products.json:', error);
-}
-
-app.get('/realtimeproducts', (req, res) => {
-    res.render('realTimeProducts', { products: productsData });
-});
-
-
-// Vista RealTimeProducts con WebSockets
-app.get('/realTimeProducts', async (req, res) => {
-    const products = await productManager.getProducts();
-    res.render('realTimeProducts', { products });
-});
-app.get('/websockets', (req, res) => {
-    res.render('websockets', { layout: 'layouts/main' });
-});
 
 server.listen(PORT, () => {
     console.log(`Servidor Express iniciado en http://localhost:${PORT}`);
